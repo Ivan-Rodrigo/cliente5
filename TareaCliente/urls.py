c2520049462ee8184bfaf93f4cc0bd06c9fce571
Ivan-Rodrigo <Rodrolp0013@gmail.com>
2020-06-09 23:01:14 -0500

    add secret

--- TareaCliente/urls.py
+++ TareaCliente/urls.py
@@ -23,15 +23,15 @@ from rest_framework import routers , serializers, viewsets
 class UserSerializer (serializers.HyperlinkedModelSerializer):
     class Meta :
         model = User
-        fields = ('url','username','email','is_staft')
+        fields = ('url','username','email','is_staff')
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializers_class = UserSerializer
 
 router = routers.DefaultRouter()
-
 router.register(r'users',UserViewSet)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^',include(router.urls))
