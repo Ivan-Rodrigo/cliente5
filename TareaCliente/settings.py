db52d78db23d8fadfe87cc089400c779b656d02d
Ivan-Rodrigo <Rodrolp0013@gmail.com>
2020-06-15 18:37:22 -0500

    add DATEBASE HEROKU

--- TareaCliente/settings.py
+++ TareaCliente/settings.py
@@ -96,8 +96,12 @@ WSGI_APPLICATION = 'TareaCliente.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8901a5rps8b76',
+        'USER': 'fxwtwfrpidgyoi',
+        'PASSWORD':'88c4a6d1605b890b53943b23f202a593e3ab839eea270dd46b07d567075f2ac1',
+        'HOST':'ec2-35-169-254-43.compute-1.amazonaws.com',
+        'PORT':'5432'
     }
 }
 
